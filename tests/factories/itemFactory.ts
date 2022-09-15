13773a38da6599734097b18a071555f85795b774
Lucas Lafèré <laferelucas@gmail.com>
2022-09-15 19:32:17 -0300

    done

---
+++ tests/factories/itemFactory.ts
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker';
+
+export default async function itemFactory () {
+    return {
+        title: faker.lorem.word(),
+        url: faker.internet.url(),
+        description: faker.lorem.paragraph(1),
+        amount: faker.datatype.number(20)
+    }
+}
